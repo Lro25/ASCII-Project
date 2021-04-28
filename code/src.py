6cab6cbcf7e79baf4df47d376b4bacde17748861
Luis R <Lr3594@outlook.com>
2021-04-27 22:20:11 -0500

    finished converting RGB values to ASCII values

--- code/src.py
+++ code/src.py
@@ -1,5 +1,28 @@
 from PIL import Image
 
-im = Image.open("../image.jpg")
+im = Image.open("../../image.jpeg")
 
-print(im.format, im.size, im.mode)
+print('Pixel Matrix Size: ' + str(im.size))
+
+pixels = []
+
+for i in range(1600):
+	pixels.append([])
+	for k in range(900):
+		pixels[i].append(im.getpixel((k,i)))
+
+
+for i in range(1600):
+        for k in range(900):
+                pixels[i][k] = int((pixels[i][k][0] + pixels[i][k][1] + pixels[i][k][2])/3)
+
+
+asc = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+for i in range(1600):
+        for k in range(900):
+                pixels[i][k] = asc[round((pixels[i][k] * 64) / 255)]
+
+for i in range(1600):
+        print(''.join(pixels[i]))
+
