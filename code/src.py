936f5654b9fa31ba25fa48b6ffd6f92fd2f11bf7
Luis R <Lr3594@outlook.com>
2021-04-29 19:53:56 -0500

    Got code to work with taking average of RGB values

--- code/src.py
+++ code/src.py
@@ -1,28 +1,37 @@
 from PIL import Image
 
-im = Image.open("../../image.jpeg")
+im_pre = Image.open("../../image2.jpeg")
 
-print('Pixel Matrix Size: ' + str(im.size))
+FACT = int(input('Input Resize Factor: '))
+
+sym_repeat_fact= int(input('Repeat symbol how many times: '))
+
+im = im_pre.resize((im_pre.size[0]//FACT,im_pre.size[1]//FACT))
+
+size = im.size
+
+print('Pixel Matrix Size: ' + str(size))
 
 pixels = []
 
-for i in range(1600):
+for i in range(size[1]):
 	pixels.append([])
-	for k in range(900):
+	for k in range(size[0]):
 		pixels[i].append(im.getpixel((k,i)))
 
 
-for i in range(1600):
-        for k in range(900):
+for i in range(size[1]):
+        for k in range(size[0]):
                 pixels[i][k] = int((pixels[i][k][0] + pixels[i][k][1] + pixels[i][k][2])/3)
 
 
-asc = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+asc = '`^\\",:;Il!i~+_-?][}{1)(|\\\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 
-for i in range(1600):
-        for k in range(900):
-                pixels[i][k] = asc[round((pixels[i][k] * 64) / 255)]
 
-for i in range(1600):
+for i in range(size[1]):
+        for k in range(size[0]):
+                pixels[i][k] = asc[round((pixels[i][k] * 64) / 255)] * sym_repeat_fact
+
+for i in range(size[1]):
         print(''.join(pixels[i]))
 
