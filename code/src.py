4af13d394076eb3a0c2d7284e81658b0644281ef
Luis R <Lr3594@outlook.com>
2021-04-29 20:27:51 -0500

    'Finished up min max brightness feature'

--- code/src.py
+++ code/src.py
@@ -20,9 +20,13 @@ for i in range(size[1]):
 		pixels[i].append(im.getpixel((k,i)))
 
 
+# for i in range(size[1]):
+#         for k in range(size[0]):
+#                 pixels[i][k] = round((pixels[i][k][0] + pixels[i][k][1] + pixels[i][k][2])/3)
+
 for i in range(size[1]):
         for k in range(size[0]):
-                pixels[i][k] = int((pixels[i][k][0] + pixels[i][k][1] + pixels[i][k][2])/3)
+                pixels[i][k] = round((max(pixels[i][k]) + min(pixels[i][k])) / 2)
 
 
 asc = '`^\\",:;Il!i~+_-?][}{1)(|\\\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
